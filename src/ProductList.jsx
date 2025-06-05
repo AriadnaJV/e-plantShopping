6d2dea9fcee8a80b43efa02749684fc32e924291
AriadnaJV <ariadnavianna@gmail.com>
2025-06-04 23:59:26 -0400

    button fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -325,7 +325,7 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-price">{plant.cost}</div> {/* Display plant cost */}
                                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
                                         <button
-                                            className="product-button"
+                                            className={addedToCart[plant.name] ? "product-button.added-to-cart" : "product-button"}
                                             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                                         >
                                             Add to Cart
