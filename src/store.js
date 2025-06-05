e9dc6b71cb731cc7842ba8a5f8a15fc1f16c032a
AriadnaJV <ariadnavianna@gmail.com>
2025-06-04 23:37:32 -0400

    Task 4 added

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+import { configureStore } from '@reduxjs/toolkit'; //set up the Redux store.
+import cartReducer from './CartSlice'; //manages the state slice related to the shopping cart.
  const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
+export default store;
